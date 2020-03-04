10a907c1ad56105932fab423730d3d34a4b91a77
zheng <1105946494@qq.com>
2020-03-04 19:53:39 +0800

    创建<Layout>

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,7 @@
-<template >
-  <div class="nav-wrapper">
-    <div class="content">Money.vue</div>
-    <Nav />
-  </div>
+<template>
+  <Layout>
+    <p>Money.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,14 +11,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid red;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  overflow: auto;
-  flex-grow: 1;
-}
 </style>
