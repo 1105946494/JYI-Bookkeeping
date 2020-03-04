aaf51e5b31a566e0d13cb4edfa719a2ae6d90cb2
zheng <1105946494@qq.com>
2020-03-04 18:41:43 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>页面不存在404</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
