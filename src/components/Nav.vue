a5853ea50f7ca52d01f990163ce6db55cd70dd69
zheng <1105946494@qq.com>
2020-03-04 23:38:17 +0800

    创建<icon />

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money" />记账
+    </router-link>
+    <router-link to="/labels">
+      <Icon name="label" />标签
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics" />统计
+    </router-link>
   </div>
 </template>
 
@@ -12,5 +18,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
