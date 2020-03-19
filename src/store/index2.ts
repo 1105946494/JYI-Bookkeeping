483a0c8c57f07f637d87d42a807063d7a5f8d9b9
zheng <1105946494@qq.com>
2020-03-19 15:47:48 +0800

    模块化的store 删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,33 +1,8 @@
-import tagLIstModel from "@/models/tagLIstModel";
-import recordlist from "@/models/recordlist";
+import recordStore from "@/store/recordStore";
+import tagStore from "@/store/tagStore";
 
 const store = {
-  //record store
-  recordList: recordlist.fetch(),
-  createRecord: (record: RecordItem) => recordlist.create(record),
-
-  //tag store
-  tagList: tagLIstModel.fetch(),
-  findTag(id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-  createTag: (name: string) => {
-    const message = tagLIstModel.create(name);
-    if (message === "duplicated") {
-      window.alert("标签名重复");
-    } else if (message === "success") {
-      return;
-    }
-  },
-  removeTag: (id: string) => {
-    if (tagLIstModel.remove(id)) {
-      return true;
-    } else {
-      return false;
-    }
-  },
-  updateTag: (id: string, name: string) => {
-    return tagLIstModel.update(id, name);
-  }
+  ...recordStore,
+  ...tagStore
 };
 export default store;
