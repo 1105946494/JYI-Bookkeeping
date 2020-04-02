c44492f30e463d67db2f32f22434c09eae17a8e6
zheng <51278829+1105946494@users.noreply.github.com>
2020-04-02 20:37:05 +0800

    Add files via upload

---
+++ dist/precache-manifest.2f634b1db6e894d1450238b6371018a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505274536c7e057787ee",
+    "url": "/JYI-Bookkeeping/dist/css/app.89717e1d.css"
+  },
+  {
+    "revision": "48f2fe446b4650d4d804c14dcec52893",
+    "url": "/JYI-Bookkeeping/dist/index.html"
+  },
+  {
+    "revision": "505274536c7e057787ee",
+    "url": "/JYI-Bookkeeping/dist/js/app.f4ee91c0.js"
+  },
+  {
+    "revision": "671155e99d2907e822a9",
+    "url": "/JYI-Bookkeeping/dist/js/chunk-vendors.86ef745a.js"
+  },
+  {
+    "revision": "d64761912105d2fb52985f5ca3784a0d",
+    "url": "/JYI-Bookkeeping/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JYI-Bookkeeping/dist/robots.txt"
+  }
+]);
